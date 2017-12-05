f7524c8478bacc91bcbb58ccf99bc4f8df099e03
cscotty <cscottbaum@gmail.com>
2017-12-05 13:17:49 -0700

    Add files via upload
    Added gridview and add new job form

---
+++ Jobs.aspx.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class Jobs : System.Web.UI.Page
+{
+     
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
+
+    protected void GridView1_RowUpdating(object sender, GridViewUpdateEventArgs e)
+    {
+        gvJobs.DataBind();
+    }
+
+    protected void LinkButton1_Click(object sender, EventArgs e)
+    {
+        Response.Redirect("NewJobForm.aspx");
+
+    }
+}
