bcf3d681fa37493e00b20e350291915ff6a8054e
alyssawinn <32552117+alyssawinn@users.noreply.github.com>
2017-12-07 13:11:08 -0700

    added login schtuff

---
+++ SultansOfServer_Project/Admin/ManageRoles.aspx
@@ -0,0 +1,54 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/MasterPage.master" AutoEventWireup="true" CodeFile="ManageRoles.aspx.cs" Inherits="Admin_ManageRoles" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="ContentPlaceHolder1" Runat="Server">
+
+    <h2><%: Title %></h2>
+    <p class="text-danger">
+        <asp:Literal runat="server" ID="StatusMessage" />
+    </p>
+
+    <div class="form-horizontal">
+
+        <h3>Add New Role</h3>
+
+        <asp:TextBox ID="txtAddRole" runat="server"></asp:TextBox>
+        <asp:Button ID="btnAddRole" runat="server" Text="Add Role" OnClick="btnAddRole_Click" />
+                       
+        <h3>View/Update Roles</h3>
+
+        <asp:GridView ID="gvwRoles" runat="server" DataKeyNames ="Id" AutoGenerateColumns="False" OnRowDeleting="gvwRoles_RowDeleting" OnRowEditing="gvwRoles_RowEditing" OnRowCancelingEdit="gvwRoles_RowCancelingEdit" OnRowUpdating="gvwRoles_RowUpdating">
+            <Columns>
+                <asp:TemplateField HeaderText="RoleName" SortExpression="Name">
+                    <EditItemTemplate>
+                        <asp:TextBox ID="TextBox1" runat="server" Text='<%# Bind("Name") %>'></asp:TextBox>
+                    </EditItemTemplate>
+                    <ItemTemplate>
+                        <asp:Label ID="Label1" runat="server" Text='<%# Bind("Name") %>'></asp:Label>
+                    </ItemTemplate>
+                </asp:TemplateField>
+                <asp:ButtonField CommandName="Delete" Text="Delete" />
+                <asp:TemplateField ShowHeader="False">
+                    <EditItemTemplate>
+                        <asp:LinkButton ID="LinkButton1" runat="server" CausesValidation="True" CommandName="Update" Text="Update"></asp:LinkButton>
+                        &nbsp;<asp:LinkButton ID="LinkButton2" runat="server" CausesValidation="False" CommandName="Cancel" Text="Cancel"></asp:LinkButton>
+                    </EditItemTemplate>
+                    <ItemTemplate>
+                        <asp:LinkButton ID="LinkButton1" runat="server" CausesValidation="False" CommandName="Edit" Text="Change Name"></asp:LinkButton>
+                    </ItemTemplate>
+                </asp:TemplateField>
+            </Columns>
+        </asp:GridView>
+
+        <h3>View/Update User Roles</h3>
+
+        <h4>Users</h4>
+        <asp:DropDownList ID="ddlUsers" runat="server" AutoPostBack="True"></asp:DropDownList>
+
+        <h4>User Roles</h4>
+        <asp:CheckBoxList ID="cblUserRoles" runat="server"></asp:CheckBoxList>
+        <asp:Button ID="btnUpdateRoles" runat="server" Text="Update Roles" OnClick="btnUpdateRoles_Click" />
+       
+    </div>
+
+</asp:Content>
+
---
+++ SultansOfServer_Project/Admin/ManageRoles.aspx.cs
@@ -0,0 +1,161 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.Owin.Security;
+using IdentityEF;
+
+public partial class Admin_ManageRoles : System.Web.UI.Page
+{
+    //create a RoleManager variable
+    IdentityEF.RoleManager rolemanager;
+
+    //create a UserManager variable
+    IdentityEF.UserManager usermanager;
+    
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        //instantiate usermanager and rolemanager
+        rolemanager = new IdentityEF.RoleManager();
+        usermanager = new IdentityEF.UserManager();
+
+       
+    }
+
+    protected void Page_PreRender(object sender, EventArgs e)
+    {
+        //populate roles GridView
+        gvwRoles.DataSource = rolemanager.Roles.ToList();
+        gvwRoles.DataBind();
+
+        //populate list of users in dropdownlist
+        ddlUsers.DataSource = usermanager.Users.ToList();
+        ddlUsers.DataTextField = "UserName";
+        ddlUsers.DataValueField = "Id";
+        if(!IsPostBack)
+         ddlUsers.DataBind();
+
+        //populate checkboxlist with roles
+        cblUserRoles.DataSource = rolemanager.Roles.ToList();
+        cblUserRoles.DataTextField = "Name";
+        cblUserRoles.DataValueField = "Name";
+        cblUserRoles.DataBind();
+
+        //check the roles that the selected user is in
+        string userID = ddlUsers.SelectedValue;
+
+        //loop through roles checkboxlist
+        foreach (ListItem li in cblUserRoles.Items)
+        {
+            if (usermanager.IsInRole(userID, li.Value))
+            {
+                li.Selected = true;
+            }
+        }
+
+
+        //usermanager.IsInRole();
+    }
+
+
+
+        protected void btnAddRole_Click(object sender, EventArgs e)
+    {
+        if (!rolemanager.RoleExists(txtAddRole.Text))
+        {
+            IdentityRole newRole = new IdentityRole(txtAddRole.Text);
+            IdentityResult result = rolemanager.Create(newRole);
+
+            //if the role was created, notify user
+            if (result.Succeeded)
+            {
+                StatusMessage.Text = "Role created";
+                txtAddRole.Text = "";
+            }
+
+            else
+            {
+                StatusMessage.Text = result.Errors.FirstOrDefault();
+            }
+        }
+          
+    }
+
+    protected void gvwRoles_RowDeleting(object sender, GridViewDeleteEventArgs e)
+    {
+        string roleId = gvwRoles.DataKeys[e.RowIndex].Value.ToString();
+        IdentityRole roleToDelete = rolemanager.FindById(roleId);
+
+        if (roleToDelete != null)
+        {
+            IdentityResult result = rolemanager.Delete(roleToDelete);
+            StatusMessage.Text = result.Succeeded ? "Role deleted" : result.Errors.FirstOrDefault();
+        }
+        
+    }
+
+    protected void gvwRoles_RowEditing(object sender, GridViewEditEventArgs e)
+    {
+        //set EditIndex of GridView to row that was clicked
+        gvwRoles.EditIndex = e.NewEditIndex;
+     
+    }
+
+    protected void gvwRoles_RowCancelingEdit(object sender, GridViewCancelEditEventArgs e)
+    {
+        //set editindex of gridview back to -1
+        gvwRoles.EditIndex = -1;
+    }
+
+    protected void gvwRoles_RowUpdating(object sender, GridViewUpdateEventArgs e)
+    {
+        //get role id
+        string roleId = gvwRoles.DataKeys[e.RowIndex].Value.ToString();
+
+        //get new role name from textbox
+        TextBox txtRoleName = (TextBox)gvwRoles.Rows[e.RowIndex].FindControl("TextBox1");
+
+        string newRoleName = txtRoleName.Text;
+
+        IdentityRole roleToUpdate = rolemanager.FindById(roleId);
+
+        //update the role if it was found
+        if (roleToUpdate != null)
+        {
+            roleToUpdate.Name = newRoleName;
+            IdentityResult result = rolemanager.Update(roleToUpdate);
+            StatusMessage.Text = result.Succeeded ? "Role updated." : result.Errors.FirstOrDefault();
+        }
+
+        //reset the edit index back to -1
+        gvwRoles.EditIndex = -1;
+    }
+
+    protected void btnUpdateRoles_Click(object sender, EventArgs e)
+    {
+        //get userid
+        string userID = ddlUsers.SelectedValue;
+
+        //loop through the roles list and add/remove user from roles as necessary
+        foreach(ListItem li in cblUserRoles.Items)
+        {
+            if (usermanager.IsInRole(userID, li.Value) && li.Selected == false)
+            {
+                //remover user from role
+                IdentityResult resultRemove = usermanager.RemoveFromRole(userID, li.Value);
+                StatusMessage.Text = resultRemove.Succeeded ? "Role removed" : resultRemove.Errors.FirstOrDefault();
+            }
+            else if (!usermanager.IsInRole(userID, li.Value) && li.Selected == true)
+            {
+                //remover user from role
+                IdentityResult resultAdd = usermanager.AddToRole(userID, li.Value);
+                StatusMessage.Text = resultAdd.Succeeded ? "Role added" : resultAdd.Errors.FirstOrDefault();
+            }
+
+        }
+    }
+}
