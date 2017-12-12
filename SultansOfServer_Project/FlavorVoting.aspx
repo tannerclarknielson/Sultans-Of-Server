574ac4d2a847285114313212f247da25901b31b8
tannerclarknielson <tannerclarknielson@gmail.com>
2017-12-11 18:06:22 -0700

    voting now works
    voting now works

--- SultansOfServer_Project/FlavorVoting.aspx
+++ SultansOfServer_Project/FlavorVoting.aspx
@@ -5,9 +5,40 @@
 </asp:Content>
 <asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" Runat="Server">
     <h1 class="underlined">Vote for your favorite flavor!</h1>
-    <asp:SqlDataSource ID="SqlDataSource1" runat="server" ConnectionString="<%$ ConnectionStrings:5050_sultans_serverConnectionString %>" SelectCommand="SELECT [Flavor] FROM [Flavors]"></asp:SqlDataSource>
-    <asp:RadioButtonList ID="RadioButtonList1" runat="server" DataSourceID="SqlDataSource1" DataTextField="Flavor" DataValueField="Flavor"></asp:RadioButtonList>
-    <asp:Button ID="Button1" runat="server" Text="Vote" OnClick="Button1_Click1" />
-    <asp:Label ID="Label1" runat="server" Text=""></asp:Label>
+    <asp:Panel ID="Panel1" runat="server">
+        <asp:SqlDataSource ID="SqlDataSource1" runat="server" ConnectionString="<%$ ConnectionStrings:5050_sultans_serverConnectionString %>" SelectCommand="SELECT [Flavor] FROM [Flavors]"></asp:SqlDataSource>
+        <asp:RadioButtonList ID="RadioButtonList1" runat="server" DataSourceID="SqlDataSource1" DataTextField="Flavor" DataValueField="Flavor"></asp:RadioButtonList>
+        <asp:Button ID="Button1" runat="server" Text="Vote" OnClick="Button1_Click1" />
+    </asp:Panel>
+    <asp:Panel ID="Panel2" runat="server">
+        <asp:Label ID="Label1" runat="server" Text=""></asp:Label>
+        <div class="row">
+            <div class="col-sm-2 col-xs-6">
+                <p style="text-align:center;">Aggie Blue Mint</p>
+                <p style="text-align:center;"><asp:Label ID="Label2" runat="server" Text="0"></asp:Label></p>
+            </div>
+            <div class="col-sm-2 col-xs-6">
+                <p style="text-align:center;">Cookies & Cream</p>
+                <p style="text-align:center;"><asp:Label ID="Label3" runat="server" Text="0"></asp:Label></p>
+            </div>
+            <div class="col-sm-2 col-xs-6">
+                <p style="text-align:center;">True Aggie Night<p>
+                <p style="text-align:center;"><asp:Label ID="Label4" runat="server" Text="0"></asp:Label></p>
+            </div>
+            <div class="col-sm-2 col-xs-6">
+                <p style="text-align:center;">Cookie Dough</p>
+                <p style="text-align:center;"><asp:Label ID="Label5" runat="server" Text="0"></asp:Label></p>
+            </div>
+            <div class="col-sm-2 col-xs-6">
+                <p style="text-align:center;">Aggie Bull Tracks</p>
+                <p style="text-align:center;"><asp:Label ID="Label6" runat="server" Text="0"></asp:Label></p>
+            </div>
+            <div class="col-sm-2 col-xs-6">
+                <p style="text-align:center;">Chocolate Chip Mint</p>
+                <p style="text-align:center;"><asp:Label ID="Label7" runat="server" Text="0"></asp:Label></p>
+            </div>
+        </div>
+        <asp:Label ID="lblResults" runat="server" Text=""></asp:Label>
+    </asp:Panel>
 </asp:Content>
 
--- SultansOfServer_Project/FlavorVoting.aspx.cs
+++ SultansOfServer_Project/FlavorVoting.aspx.cs
@@ -10,30 +10,28 @@ public partial class FlavorVoting : System.Web.UI.Page
 {
     protected void Page_Load(object sender, EventArgs e)
     {
-        
+        Panel2.Visible = false;
     }
 
     protected void Button1_Click1(object sender, EventArgs e)
     {
-        Label1.Text = RadioButtonList1.SelectedValue;
-
-        string updateSQL = "UPDATE Flavors";
-        updateSQL += "SET Votes = Votes + 1";
-        updateSQL += "WHERE Flavor = '@flavor'";
+        //update the votes for each flavor in the database
+        string updateSQL = "UPDATE Flavors ";
+        updateSQL += "SET Votes = Votes + 1 ";
+        updateSQL += "WHERE Flavor = @Flavor";
 
         SqlConnection con = new SqlConnection("Data Source=stairway.usu.edu; Initial Catalog=5050_sultans_server;User ID=5050_sultans_server; Password=aggieicecream");
         SqlCommand cmd = new SqlCommand(updateSQL, con);
 
         SqlParameter param = new SqlParameter();
-        cmd.Parameters.AddWithValue("@flavor", RadioButtonList1.SelectedValue.ToString());
-     
-       
+        cmd.Parameters.AddWithValue("@Flavor", RadioButtonList1.SelectedValue);
+
         int added = 0;
         try
         {
             con.Open();
             added = cmd.ExecuteNonQuery(); //use for insert, update, delete commands
-            Label1.Text = "Thank you for submitting your new flavor idea!";
+            Label1.Text = "Thank you for submitting your vote, here are the current results!";
         }
         catch (Exception err)
         {
@@ -45,6 +43,171 @@ public partial class FlavorVoting : System.Web.UI.Page
             con.Close();
         }
 
-        //Panel1.Visible = false;
+        Panel1.Visible = false;
+        Panel2.Visible = true;
+
+        // selecting data from the database for Aggie Ice cream
+        string selectSQL = "SELECT Votes FROM Flavors WHERE FlavorID = 1";
+
+        SqlConnection con2 = new SqlConnection("Data Source=stairway.usu.edu; Initial Catalog=5050_sultans_server;User ID=5050_sultans_server; Password=aggieicecream");
+        SqlCommand cmd2 = new SqlCommand(selectSQL, con2);
+        SqlDataReader reader;
+
+        try
+        {
+            con2.Open();
+            reader = cmd2.ExecuteReader();
+            reader.Read();
+
+            Label2.Text = reader["Votes"].ToString();
+
+            reader.Close();
+        }
+        catch(Exception err)
+        {
+            lblResults.Text = "Error selecting the votes. ";
+            lblResults.Text += err.Message;
+        }
+        finally
+        {
+            con2.Close();
+        }
+
+        // selecting data from the database for Aggie Ice cream
+        string selectSQL2 = "SELECT Votes FROM Flavors WHERE FlavorID = 2";
+
+        SqlConnection con3 = new SqlConnection("Data Source=stairway.usu.edu; Initial Catalog=5050_sultans_server;User ID=5050_sultans_server; Password=aggieicecream");
+        SqlCommand cmd3 = new SqlCommand(selectSQL2, con3);
+        SqlDataReader reader2;
+
+        try
+        {
+            con3.Open();
+            reader2 = cmd3.ExecuteReader();
+            reader2.Read();
+
+            Label3.Text = reader2["Votes"].ToString();
+
+            reader2.Close();
+        }
+        catch (Exception err)
+        {
+            lblResults.Text = "Error selecting the votes. ";
+            lblResults.Text += err.Message;
+        }
+        finally
+        {
+            con3.Close();
+        }
+
+        // selecting data from the database for Aggie Ice cream
+        string selectSQL3 = "SELECT Votes FROM Flavors WHERE FlavorID = 3";
+
+        SqlConnection con4 = new SqlConnection("Data Source=stairway.usu.edu; Initial Catalog=5050_sultans_server;User ID=5050_sultans_server; Password=aggieicecream");
+        SqlCommand cmd4 = new SqlCommand(selectSQL3, con4);
+        SqlDataReader reader3;
+
+        try
+        {
+            con4.Open();
+            reader3 = cmd4.ExecuteReader();
+            reader3.Read();
+
+            Label4.Text = reader3["Votes"].ToString();
+
+            reader3.Close();
+        }
+        catch (Exception err)
+        {
+            lblResults.Text = "Error selecting the votes. ";
+            lblResults.Text += err.Message;
+        }
+        finally
+        {
+            con4.Close();
+        }
+
+        // selecting data from the database for Aggie Ice cream
+        string selectSQL4 = "SELECT Votes FROM Flavors WHERE FlavorID = 4";
+
+        SqlConnection con5 = new SqlConnection("Data Source=stairway.usu.edu; Initial Catalog=5050_sultans_server;User ID=5050_sultans_server; Password=aggieicecream");
+        SqlCommand cmd5 = new SqlCommand(selectSQL4, con5);
+        SqlDataReader reader4;
+
+        try
+        {
+            con5.Open();
+            reader4 = cmd5.ExecuteReader();
+            reader4.Read();
+
+            Label5.Text = reader4["Votes"].ToString();
+
+            reader4.Close();
+        }
+        catch (Exception err)
+        {
+            lblResults.Text = "Error selecting the votes. ";
+            lblResults.Text += err.Message;
+        }
+        finally
+        {
+            con5.Close();
+        }
+
+        // selecting data from the database for Aggie Ice cream
+        string selectSQL5 = "SELECT Votes FROM Flavors WHERE FlavorID = 5";
+
+        SqlConnection con6 = new SqlConnection("Data Source=stairway.usu.edu; Initial Catalog=5050_sultans_server;User ID=5050_sultans_server; Password=aggieicecream");
+        SqlCommand cmd6 = new SqlCommand(selectSQL5, con6);
+        SqlDataReader reader5;
+
+        try
+        {
+            con6.Open();
+            reader5 = cmd6.ExecuteReader();
+            reader5.Read();
+
+            Label6.Text = reader5["Votes"].ToString();
+
+            reader5.Close();
+        }
+        catch (Exception err)
+        {
+            lblResults.Text = "Error selecting the votes. ";
+            lblResults.Text += err.Message;
+        }
+        finally
+        {
+            con6.Close();
+        }
+
+        // selecting data from the database for Aggie Ice cream
+        string selectSQL6 = "SELECT Votes FROM Flavors WHERE FlavorID = 6";
+
+        SqlConnection con7 = new SqlConnection("Data Source=stairway.usu.edu; Initial Catalog=5050_sultans_server;User ID=5050_sultans_server; Password=aggieicecream");
+        SqlCommand cmd7 = new SqlCommand(selectSQL6, con7);
+        SqlDataReader reader6;
+
+        try
+        {
+            con7.Open();
+            reader6 = cmd7.ExecuteReader();
+            reader6.Read();
+
+            Label7.Text = reader6["Votes"].ToString();
+
+            reader6.Close();
+        }
+        catch (Exception err)
+        {
+            lblResults.Text = "Error selecting the votes. ";
+            lblResults.Text += err.Message;
+        }
+        finally
+        {
+            con7.Close();
+        }
+
+        Panel2.Visible = true;
     }
 }
